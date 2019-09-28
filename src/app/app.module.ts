5807f0fdf2cb76719e37b2ee374acc59a9e67636
Jonathan Lee <jtwlee@edu.uwaterloo.ca>
2019-09-28 17:34:34 -0400

    add main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { SignUpComponent } from './sign-up/sign-up.component';
 import { SearchComponent } from './search/search.component';
 import { RecommendationComponent } from './recommendation/recommendation.component';
 import { FilterComponent } from './filter/filter.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material'
+import { MatFormFieldModule } from '@angular/material'
+import { MatInputModule } from '@angular/material'
+import { MatRippleModule } from '@angular/material'
+
+import { MatToolbarModule } from '@angular/material'
+import { MatCardModule } from '@angular/material/card'
+import { MatGridListModule } from '@angular/material/grid-list'
 
 @NgModule({
   declarations: [
@@ -18,7 +30,31 @@ import { FilterComponent } from './filter/filter.component';
     FilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent},
+      { path: 'search', component: SearchComponent},
+      { path: 'signup', component: SignUpComponent},
+      { path: 'filter', component: FilterComponent},
+      { path: 'recommendation', component: RecommendationComponent},
+    ]),
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
