c409a5cb1689809b5a94c2559f77715c65b5a879
Yunna <wonyu@shernet.sheridancollege.ca>
2019-09-28 16:15:08 -0400

    publish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SearchComponent } from './search/search.component';
+import { RecommendationComponent } from './recommendation/recommendation.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SignUpComponent,
+    SearchComponent,
+    RecommendationComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
